c3029e35cbe4700bb572d5487049a595a4f951c0
Gigi Bachtel <gbachtel@ucsc.edu>
2019-05-26 22:22:05 -0700

    Slight updates to Part A font size

--- CMPM 163 Homework 3/Assets/Scripts/AudioPeer.cs
+++ CMPM 163 Homework 3/Assets/Scripts/AudioPeer.cs
@@ -2,37 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 // create this required component....
 [RequireComponent (typeof (AudioSource))]
 
 public class AudioPeer : MonoBehaviour {
 
-	// need to instantiate an audio source and array of samples to store the fft data.
+    AudioSource _audioSource;
 
-	AudioSource _audioSource;
-
-	// NOTE: make this a 'static' float so we can access it from any other script.
-	public static float[] spectrumData = new float[512];
+    // NOTE: make this a 'static' float so we can access it from any other script.
+    public static float[] spectrumData = new float[512];
 
 
 
 	// Use this for initialization
 	void Start () {
 		
-		_audioSource = gameObject.GetComponent<AudioSource> ();	
+		_audioSource = gameObject.GetComponent<AudioSource> ();
 
-	}
+    }
 
 
 	// Update is called once per frame
 	void Update () {
 
-		GetSpectrumAudioSource ();
+        GetSpectrumAudioSource();
+    }
 
-	}
 
-
-	void GetSpectrumAudioSource()
+    void GetSpectrumAudioSource()
 	{
 		// this method computes the fft of the audio data, and then populates spectrumData with the spectrum data.
 		_audioSource.GetSpectrumData (spectrumData, 0, FFTWindow.Hanning);
